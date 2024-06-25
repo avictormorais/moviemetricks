6fd30620d8852351bf33d962424772b6c04464b6
Victor Morais <110771678+avictormorais@users.noreply.github.com>
2024-06-25 14:07:01 -0300

    Criando componente de detalhes de episódio / mudando estrutura da rota de episódio para mostrar o novo componente

--- src/components/Sections/EpisodeDetails.jsx
+++ src/components/Sections/EpisodeDetails.jsx
@@ -1,24 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import styles from './EpisodeDetails.module.css';
+import React from "react";
+import styles from "./EpisodeDetails.module.css";
+import { FaClock, FaStar, FaCalendar } from "react-icons/fa6";
+import Person from "../Person";
+import api from "../../services/api";
+import { useEffect, useState } from "react";
 
-function EpisodeDetails({ episode }){
-  console.log(episode)
+function EpisodeDetails({ episode }) {
+  const [credits, setCredits] = useState(null);
+
+  useEffect(() => {
+      api.get(`tmdb/credits?id=${episode.show_id}&season_number=${episode.season_number}&episode_number=${episode.episode_number}`)
+      .then((response) => {
+        setCredits(response.data)
+      })
+    }, [episode])
 
   return (
-    <div className={styles.divEp}>
-      {episode ? (
-        <>
-          <div className={styles.divPreview}>
-            <img src="" alt="" />
-          </div>
-          <div className={styles.divDetails}>
-            <h1 className={styles.EpName}>Nome ep</h1>
-            <p className={styles.overview}>Resumo</p>
-            <div className={styles.divInfos}></div>
-          </div>
-        </>
-      ) : null}
-    </div>
+    <>
+      <div className={styles.divEp}>
+        {episode ? (
+          <>
+            <div className={styles.divPreview}>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${episode.still_path}`}
+                alt=""
+              />
+            </div>
+            <div className={styles.divDetails}>
+              <h1 className={styles.EpName}>{episode.name}</h1>
+              <p className={styles.overview}>{episode.overview}</p>
+              <div className={styles.divInfos}>
+                <span>
+                  <FaClock />
+                  {episode.runtime} min
+                </span>
+                <span>
+                  <FaStar />
+                  {episode.vote_average.toFixed(1)}
+                </span>
+                <span>
+                  <FaCalendar />
+                  {episode.air_date}
+                </span>
+              </div>
+            </div>
+          </>
+        ) : null}
+      </div>
+      <div className={styles.persons}>
+        {credits && (
+          <>
+            {credits.cast.map((person, index) => (
+              person.profile_path && <Person key={index} name={person.name} image={person.profile_path} id={person.id} role={person.character.split('(')[0]}/>
+            ))}
+            {credits.guest_stars.map((person, index) => (
+              person.profile_path && <Person key={index} name={person.name} image={person.profile_path} id={person.id} role={person.character.split('(')[0]}/>
+            ))}
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
